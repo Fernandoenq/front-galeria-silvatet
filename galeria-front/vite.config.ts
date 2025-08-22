c80aec74d16e211135789c9f2f0c3f5c84efb1b0
mateus silva <teuzmachado5@gmail.com>
2025-08-21 21:01:45 -0300

    tropa de bigode

--- galeria-front/vite.config.ts
+++ galeria-front/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // permite acesso externo
-    port: 5174,
+    port: 3333,
     allowedHosts: ['ec2-15-228-149-9.sa-east-1.compute.amazonaws.com'], // libera seu domínio público da EC2
   }
 })
